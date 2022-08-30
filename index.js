e79c3830d2ce3316c8d0600dd7a06def506290d2
Aman Jaiswal <aman.jaiswal0612@gmail.com>
2022-08-30 20:36:50 +0530

    d

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get("/",(req,res)=>{
 })  
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-const PORT= process.env.port||5000
+const PORT= process.env.PORT||5000
 const server = app.listen(PORT, () =>
   console.log(`Server started on ${PORT}`)
 );
